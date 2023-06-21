d0fbbd949be7ebd17dbd900a86096dd19e25eebe
Christine Yoon <christineyoon@Christines-Air.fios-router.home>
2023-06-21 02:41:18 -0400

    update

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { CanvasProvider } from "./CanvasContext";
+import BrowserRouter from 'react-router-dom/BrowserRouter';
 
 ReactDOM.render(
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
   <React.StrictMode>
     <CanvasProvider>
       <App />
     </CanvasProvider>
-  </React.StrictMode>,
+  </React.StrictMode>
+  </BrowserRouter>,
   document.getElementById("root")
 );
