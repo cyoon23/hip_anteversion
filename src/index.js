129989cba81bfe153c3530a7104bab99c01dd80c
Christine Yoon <christineyoon@Christines-Air.fios-router.home>
2023-06-21 03:35:41 -0400

    massive changes

--- src/index.js
+++ src/index.js
@@ -3,15 +3,12 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { CanvasProvider } from "./CanvasContext";
-import BrowserRouter from 'react-router-dom/BrowserRouter';
 
 ReactDOM.render(
-  <BrowserRouter basename={process.env.PUBLIC_URL}>
   <React.StrictMode>
     <CanvasProvider>
       <App />
     </CanvasProvider>
-  </React.StrictMode>
-  </BrowserRouter>,
+  </React.StrictMode>,
   document.getElementById("root")
 );
